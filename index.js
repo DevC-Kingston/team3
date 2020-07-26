d9fe924ff870dd255abee858c4e4d9ac6fa78207
lutajpeters <allutap@yahoo.com>
2020-07-26 17:48:22 -0500

    Add files via upload
    Added button options and post back handler.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const PAGE_ACCESS_TOKEN = "EAAeo5mY4LCkBANgbcC4izrjzmZCBW2Tm1YFezDJ4NnQEZC4Ahjo3VCyTJ86chhrYCgS0bAD2ZC3SRP2ITDNIqF6hZCji42EnyHpZBsm4HxnHYzrHZApB5ctKFQaeXLk74tAZBNgpTwXa66HHSUsY4IArqfdr22faxDfPbG9BZCDWnuOvLtbQYlrE"
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -9,7 +12,7 @@ const app = express()
 app.set('port', (process.env.PORT || 5000))
 
 //allow data processing
-app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
@@ -19,19 +22,13 @@ app.get('/', function(req, res) {
 })
 
 
-//let app_token = "EAAEkXLChDsMBAHE6xd2tt86DYcU9t9AVY1JHXfpmZCXn6ZCzmMS06ju4TS6tHBZAIU8jPOqat44DZAZBh8LQSZA0IHBt8p4Yi1AFpeuQFpqG381ZAkSHjccVedAiSHL0TUIMb2cUCkMiLDx4HY0KaL6fYwIhZC4fL8wDd8CSdxICC3TeZBgwAO8Ys"
+let greetings = ["Hi", "HI", "hi", "Hello", "HELLO", "hello", "Hi How are you", "Hello How are you", " Hi what's up", " Hello what's up", "Sup", "SUP", "sup", "Hola"];
 
-//let barnaby_token = "EAAEU5gpE1W0BAFtP2XpK548KUPm5pRDFreDemZALZAuuOUa6DW9O6oQZBkpRaZBnLZAyGI0ob2HJWtZBTjtO81gDOLM7ZCURoGLE5w11SOe8hy6wCN3v6uNxBnUKRedXTZBBDi7e68GnbE3zCYKvqIA2e33lLR2aZCqXWZBUhTM9wRQ1cWzM1DCtQQ"
-
-let q_token = "EAAEkXLChDsMBAHPeqbxE2SZAZA8CZCJxsAd78TJhPEhkgmtiADSO7ids8wJmRjSJZA0MtLBWKo2QwHAifmXUL5AQ55UYdL9QKnHWJiWEDRxhPaz3iiOa8C4u2oBRSHYp6K17W0d83DjqvieQGTJYcmmAnMZCuRrkd8d6yyt08ZBatJwQt6KH93"
-
-let greetings = ["Hi", "HI", "hi", "Hello", "HELLO", "hello", "Hi How are you", "What's up", "Sup", "SUP", "sup"];
-
-let greeting_response = ["Hi I'm Barnaby, How can I assist?", "Hello, how may I help you today?", "Hi there. How may I help you today?"]
+let greeting_response = ["Hi I'm Barnaby, how can I assist?", "Hello I'm Barnaby, how may I help you today?", "Hi there I'm Barnaby, how may I help you today?"]
 
 let q_reporting = ["Report account missuse","Report account theft",	"Report account fraud"]
 
-let q_report = ["Report account missuse","Report account theft","Report account fraud","report account missuse","report account theft","report account fraud", "REPORT ACCOUNT MISSUSE", "REPORT ACCOUNT FRAUD", "REPORT ACCOUNT THEFT"]
+let q_report = ["Report account misuse","Report account theft","Report account fraud","report account misuse","report account theft","report account fraud", "REPORT ACCOUNT MISUSE", "REPORT ACCOUNT FRAUD", "REPORT ACCOUNT THEFT", "Account misuse", "Account theft", "Account fraud", "account misuse", "account theft", "account fraud", "misuse", "theft", "fraud"]
 
 
 let q_platforms = [
@@ -41,6 +38,12 @@ let q_platforms = [
 	"TIKTOK",
 	"TINDER",
 	"SNAPCHAT",
+	"Instagram",
+	"Facebook",
+	"Twitter",
+	"Tiktok",
+	"Tinder",
+	"Snapchat",
 	"instagram",
 	"facebook",
 	"twitter",
@@ -64,7 +67,7 @@ let r_platforms = [
 
 app.get('/webhook', function(req, res){
 
-	const VERIFY_TOKEN = "<AP_VERIFY_TOKEN>"
+	const VERIFY_TOKEN = "AP_VERIFY"
 
 	// Parse the query params
 	let mode = req.query['hub.mode'];
@@ -92,99 +95,330 @@ app.post('/webhook/', function(req, res){
 		if(event.message && event.message.text) {
 			let text = event.message.text
 			
-			for (var l of greetings) {
-				
-				if(text.includes(l)) { //if the message text conatins a gretting, respond with a greeting from the greeting_response array 
-					let num = Math.floor(Math.random() * greeting_response.length); //generates a random number from 0 - max of greeting_response array
-					let msg = greeting_response[num]; //sets a random response from the greeting_response array to the message variable 
-															
-					sendText(sender, msg); //sends the response to the recipient
+			messageHandler(sender, text)
 
-					let q_rep = ""
-					for(var x of q_reporting) {
-						if(q_rep === "" || q_rep.length === 0 || q_rep === null){
-							q_rep = q_rep.concat(x) //adds an initial complaint type to the empty string
-						}else{
-							q_rep = q_rep.concat(" | " + x) //adds the remaining complaint types
-						}
-					}
-					sendText(sender, q_rep); //displays the various types of complaints to the user
-
-				}
-			}
-
-
-			for(var r of q_report){
-
-				if(text.includes(r)) { //if the message text conatins a complaint type, add each platform name to the plats variable 
-					let a_rep = "Okay, I can help you with that.";
-					sendText(sender, a_rep);
-
-					a_rep = "On which social media platfrom do you have this issue?";
-					sendText(sender, a_rep);
-
-					let plats = ""
-					for(var x of r_platforms) {
-						if(plats === "" || plats.length === 0 || plats === null){
-							plats = plats.concat(x)
-						}else{
-							plats = plats.concat(" | " + x)
-						}
-					}
-
-					sendText(sender, plats); //display the various platforms to the user
-
-				}
-
-			}
-
-
-			for (var p of q_platforms){
-
-				if(text.includes(p)) { 
-					let msg = "Okay.";
-					sendText(sender, msg);
-
-					msg = "Could you confirm the user name of the account.";
-					sendText(sender, msg);
-
-				}
-
-			}
-
-
-			if(text.includes("@")) {
-
-				let msg = "Okay, we will submit the request on your behalf";
-				sendText(sender, msg);
-
-				msg = "Please complete the Consent Note Verification Form and Document Verification form";
-				sendText(sender, msg);
-
-				msg = "Here's the link: www.tiktokreportmaintain.com";
-				sendText(sender, msg);
-
-			}
-
+		}
+		else if (event.postback) {
+			//let text = JSON.stringify(event.postback)
+			//messageHandler(sender, text);
+			postbackHandler(sender, event.postback);
+			continue
 
 		}
 
 	}
+
 
 	res.sendStatus(200);
 })
 
 
 
-function sendText(sender, text){
+function postbackHandler(sender, postback_message) {
+	
+	console.log('ok');
+	let response;
+  	// Get the payload for the postback
+  	let payload = postback_message.payload;
+
+  	// Set the response based on the postback payload
+  	
+  	for(var r of q_report) {
+
+		if(payload === r) { //if the message text conatins a complaint type, display each platform name to the user
+			let a_rep = "Okay, I can help you with that.";
+			sendMessage(sender, a_rep);
+
+			a_rep = "On which social media platfrom do you have this issue?";
+			sendPlatforms(sender, a_rep); //display the various platforms to the user
+			break
+
+		}
+
+	}
+
+
+	for (var p of q_platforms){
+
+		if(payload === p) { 
+			let msg = "Okay.";
+			sendMessage(sender, msg);
+
+			msg = "Could you confirm the user name of the account?";
+			sendMessage(sender, msg);
+
+			break
+		}
+
+	}
+
+
+}
+
+
+
+
+function messageHandler(sender, received_message){
+	
+	let handleText = received_message
+	
+
+	for (var l of greetings) {
+		
+		if(handleText === l) { //if the message text conatins a gretting, respond with a greeting from the greeting_response array 
+			let num = Math.floor(Math.random() * greeting_response.length); //generates a random number from 0 - max of greeting_response array
+			let msg
+			msg = greeting_response[num]; //sets a random response from the greeting_response array to the message variable 
+			sendActions(sender, msg);
+			//sendImages(sender);
+			break
+		}
+		
+	}
+
+
+
+	for(var r of q_report) {
+
+		if(handleText === r) { //if the message text conatins a complaint type, display each platform name to the user
+			let a_rep = "Okay, I can help you with that.";
+			sendMessage(sender, a_rep);
+
+			a_rep = "On which social media platfrom do you have this issue?";
+			sendPlatforms(sender, a_rep); //display the various platforms to the user
+			break
+
+		}
+
+	}
+
+
+	for (var p of q_platforms){
+
+		if(handleText === p) {
+
+			msg = "Okay." + "\n" + "Could you confirm the user name of the account?";
+			sendMessage(sender, msg);
+
+			break
+		}
+
+	}
+
+
+	if(handleText.includes("@")) {
+		sendForm(sender);		
+	}
+
+
+	if(handleText.includes("No") || handleText.includes("no") || handleText.includes("NO")){
+		let dat = "Okay, we can skip that for now." + "\n" + "Please complete the Consent Note Verification Form and Document Verification form";
+		sendMessage(sender, dat);
+
+		dat = "Here's the link.";
+		sendLink(sender, dat);
+
+	}
+
+
+	if(handleText.includes("who are you?")){
+		genericButton(sender);
+	}
+
+
+
+	//msg = "Sorry, I didn't catch that";
+	//sendMessage(sender, msg); //sends the response to the recipient
+		
+}
+
+
+
+function sendForm(sender) {
+	let msg = "Thank you for the information, we will submit the request on your behalf.";
+	sendMessage(sender, msg);
+
+	msg = "The information will be reviewed and the necessary actions taken within the next hour.";
+	sendMessage(sender, msg);
+
+	msg = "Please complete the Consent Note Verification Form and Document Verification form";
+	sendMessage(sender, msg);
+
+	msg = "Here's the link.";
+	sendLink(sender, msg);
+
+}
+
+
+
+function sendLink(sender, text) {
+	
+	let msgD = {
+		"attachment": {
+			"type":"template",
+			"payload": {
+				"template_type": "button",
+				"text": text,
+				"buttons": [
+					{
+						"type": "web_url",
+						"url": "www.tiktokreportmaintain.com",
+						"title": "Reporting Form"
+					}
+				]
+
+			}
+		}
+	}
+
+	sendRequest(sender, msgD)
+
+}
+
+
+
+
+function sendActions(sender, text) {
+	
+	let msgA = {
+		"attachment": {
+			"type":"template",
+			"payload": {
+				"template_type": "button",
+				"text": text,
+				"buttons": [
+					{
+						"type": "postback",
+						"title": "Report account fraud",
+						"payload": "Account fraud"
+					},
+					{
+						"type": "postback",
+						"title": "Report account misuse",
+						"payload": "Account misuse"
+					},
+					{
+						"type": "postback",
+						"title": "Report account theft",
+						"payload": "Account theft"
+					}
+				]
+
+			}
+		}
+	}
+
+
+	sendRequest(sender, msgA)
+
+}
+
+
+
+function sendPlatforms(sender, text) {
+	
+	let msgP = {
+		"attachment": {
+			"type":"template",
+			"payload": {
+				"template_type": "button",
+				"text": text,
+				"buttons": [
+					{
+						"type": "postback",
+						"title": "Instagram",
+						"payload": "INSTAGRAM"
+					},
+					{
+						"type": "postback",
+						"title": "Facebook",
+						"payload": "FACEBOOK"
+					},
+					{
+						"type": "postback",
+						"title": "Tiktok",
+						"payload": "TIKTOK"
+					}
+				]
+
+			}
+		}
+	}
+
+	sendRequest(sender, msgP);
+
+}
+
+
+
+function sendImages(sender) {
+
+	let data = {
+		"attachment": {
+			"type": "image",
+			"payload" : {
+				"url": "https://caricom.org/wp-content/uploads/food-security-2.jpg"
+			}
+		}
+	}
+
+	sendRequest(sender, data)
+
+}
+
+
+
+function genericButton(sender) {
+
+	let mdata = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type":"generic",
+				"elements": [
+					{
+						"title": "Barnaby",
+						"image_url": "https://res.cloudinary.com/jerrick/image/upload/fl_progressive,q_auto,w_1024/ukcgwnavdb3ckrzmfqlf.jpg",
+						"subtitle":"Barnaby bot",
+						"buttons": [
+							{
+								"type": "web_url",
+								"url": "https://www.facebook.com/ChatBot-107724041033953/?modal=admin_todo_tour",
+								"title": "Learn more"
+							}
+						]
+					}
+				]
+			}
+		}
+	}
+
+	sendRequest(sender, mdata)
+
+}
+
+
+
+
+
+function sendMessage(sender, text){
 	let data = {text: text}
+	
+
+	sendRequest(sender, data)
+
+}
+
+
+
+function sendRequest(sender, messageText) {
 	request({
 		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token: q_token},
+		qs : {access_token: PAGE_ACCESS_TOKEN},
 		method: "POST",
 		json: {
 			recipient: {id: sender},
-			message : data
+			message : messageText
 		}
 
 	}), function(error, response, body) {
@@ -197,35 +431,7 @@ function sendText(sender, text){
 }
 
 
-
-
-/*
-function messageHandler(sender, text){
-	
-	let handlerData = {text: text}
-
-	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token: q_token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message : handlerData
-		}
-
-	}), function(error, response, body) {
-		if(error){
-			console.log("sending error")
-		}else if (response.body.error){
-			console.log("response body error")
-		}
-	}
-
-	
-}
-*/
-
-
+ 
 
 app.listen(app.get('port'), function(){
 	console.log("Port active")
@@ -243,3 +449,4 @@ app.listen(app.get('port'), function(){
 
 
 
+/**/
